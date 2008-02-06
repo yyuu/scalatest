b2854055ff5357d67c2e1a77d533d163a4bb11ea
joshcough <joshcough@fd1274fd-080f-5963-8033-43ee40fcd1de>
2008-02-06 04:34:08 +0000

    enhanced example. added before and after calls to demonstrate infoProvided.
    git-svn-id: http://scalatest.googlecode.com/svn/branches/josh-3@60 fd1274fd-080f-5963-8033-43ee40fcd1de

--- app/tests/org/scalatest/testng/example/ExampleTestNGSuite.scala
+++ app/tests/org/scalatest/testng/example/ExampleTestNGSuite.scala
@@ -1,23 +1,48 @@
 package org.scalatest.testng.example;
 
 import org.testng.annotations.Test
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.BeforeClass
+import org.testng.annotations.BeforeSuite
+import org.testng.annotations.AfterMethod
+import org.testng.annotations.AfterClass
+import org.testng.annotations.AfterSuite
+import org.scalatest.Report
+import org.scalatest.Reporter
+import org.specs.mock._
 
-class ExampleTestNGSuite extends TestNGSuite{
+class ExampleTestNGSuite extends TestNGSuite with JMocker{
 
-  @Test{val invocationCount=10}
-  def thisTestRunsTenTimes = {}
+  @AfterSuite
+  def failAfterSuite(){ throw new Exception("fail in before method") }
+
+  @BeforeMethod def passBeforeMethod(){}
+  @BeforeClass def passBeforeClass(){}
+  @BeforeSuite def passBeforeSuite(){}
   
-  @Test{val groups=Array("runMe")} def testWithException() {
-    throw new Exception("exception!!!")
+  @AfterMethod def passAfterMethod(){}
+  @AfterClass def passAfterClass(){}
+  @AfterSuite def passAfterSuite(){}
+  
+  @Test{val invocationCount=10} def thisTestRunsTenTimes = {}
+  
+  @Test{val groups=Array("runMe")} 
+  def testWithException(){ 
+    throw new Exception("exception!!!") 
+   }
+  
+  @Test{val groups=Array("runMe")} def testWithAssertFail = assert( 1 === 2, "assert fail!!!" )
+  
+  @Test{val dependsOnMethods=Array("testWithException")} def testToGetSkipped = {}
+
+  @Test
+  def testWithMocking = {
+    //val mockReporter = mock(classOf[Reporter])
+    //expect {
+    //  one(mockReporter).testSucceeded(any(classOf[Report]))
+    //}
+    //mockReporter.testSucceeded( new Report( "test", "test" ) )
   }
-  
-  @Test{val groups=Array("runMe")} def testWithAssertFail() {
-    assert( 1 === 2, "assert fail!!!" )
-  }
-  
-  @Test{val dependsOnMethods=Array("testWithException")}
-  def testToGetSkipped() = {}
-  
   
 }
 
