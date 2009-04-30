8c0b5c0ca975cc5bd5c0263c7835f22167b07806
bv <bv@fd1274fd-080f-5963-8033-43ee40fcd1de>
2009-04-30 22:31:38 +0000

    first cut at Ordinal
    git-svn-id: http://scalatest.googlecode.com/svn/branches/bv-refactorReporter@1188 fd1274fd-080f-5963-8033-43ee40fcd1de

---
+++ src/org/scalatest/Ordinal.scala
@@ -0,0 +1,42 @@
+/*
+ * Ordinal.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.scalatest
+
+// TODO: equals and hashCode
+final class Ordinal private (val runStamp: Int, stamps: Array[Int]) extends Ordered[Ordinal] {
+
+  def this(runStamp: Int) = this(runStamp, Array(0))
+
+  def next: Ordinal = {
+    val newArray = new Array[Int](stamps.length) // Can't seem to clone
+    val zipped = stamps.zipWithIndex
+    for ((num, idx) <- zipped)
+      newArray(idx) = num
+    newArray(0) += 1
+    new Ordinal(runStamp, newArray)
+  }
+
+  /*
+the first one is the ordinal for the new suite, the next one
+is the ordinal for this suite
+  */
+  def nextForNewSuite: (Ordinal, Ordinal) = {
+    val newArrayForNewSuite = new Array[Int](stamps.length + 1)
+    val newArrayForThisSuite = new Array[Int](stamps.length)
+    val zipped = stamps.zipWithIndex
+    for ((num, idx) <- zipped) {
+      newArrayForNewSuite(idx) = num
+      newArrayForThisSuite(idx) = num
+    }
+    (new Ordinal(runStamp, newArrayForNewSuite), new Ordinal(runStamp, newArrayForThisSuite))
+  }
+
+  def toList: List[Int] = stamps.toList
+
+  def compare(that: Ordinal) = 0
+}
