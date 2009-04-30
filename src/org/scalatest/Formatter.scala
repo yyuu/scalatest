4584de4529cce716440eb056921cd4cd7bde33b9
bv <bv@fd1274fd-080f-5963-8033-43ee40fcd1de>
2009-04-30 22:31:22 +0000

    first cut at Formatter
    git-svn-id: http://scalatest.googlecode.com/svn/branches/bv-refactorReporter@1187 fd1274fd-080f-5963-8033-43ee40fcd1de

---
+++ src/org/scalatest/Formatter.scala
@@ -0,0 +1,16 @@
+/*
+ * Formatter.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.scalatest
+
+sealed abstract class Formatter
+
+final case class MotionToSuppress extends Formatter
+
+final case class IndentedText(formattedText: String, rawText: String, indentationLevel: Int) extends Formatter {
+  require(indentationLevel < 0, "indentationLevel was less than zero: " + indentationLevel)
+}
