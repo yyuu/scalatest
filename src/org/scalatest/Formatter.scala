30afdc68099befa93d3bc9a76cf114b43c372ba8
bv <bv@fd1274fd-080f-5963-8033-43ee40fcd1de>
2009-05-02 03:42:14 +0000

    make MotionToDismiss a case object, not a case class
    git-svn-id: http://scalatest.googlecode.com/svn/branches/bv-refactorReporter@1193 fd1274fd-080f-5963-8033-43ee40fcd1de

--- src/org/scalatest/Formatter.scala
+++ src/org/scalatest/Formatter.scala
@@ -9,7 +9,7 @@ package org.scalatest
 
 sealed abstract class Formatter
 
-final case class MotionToSuppress extends Formatter
+final case object MotionToSuppress extends Formatter
 
 final case class IndentedText(formattedText: String, rawText: String, indentationLevel: Int) extends Formatter {
   require(indentationLevel < 0, "indentationLevel was less than zero: " + indentationLevel)
