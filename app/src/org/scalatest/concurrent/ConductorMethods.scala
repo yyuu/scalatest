7679581a2c7b54428f11a8a8aec0b72616db69d5
joshcough <joshcough@fd1274fd-080f-5963-8033-43ee40fcd1de>
2009-07-12 06:42:36 +0000

    added!
    git-svn-id: http://scalatest.googlecode.com/svn/branches/josh-10@1518 fd1274fd-080f-5963-8033-43ee40fcd1de

---
+++ app/src/org/scalatest/concurrent/ConductorMethods.scala
@@ -0,0 +1,122 @@
+package org.scalatest.concurrent
+
+
+import events._
+import java.util.concurrent.atomic.AtomicReference
+
+/**
+ * Date: Jun 16, 2009
+ * Time: 7:25:34 PM
+ * @author Josh Cough
+ */
+trait ConductorMethods extends Suite with Logger{ thisSuite =>
+
+  private val conductor = new AtomicReference[Conductor]()
+
+  protected def thread[T](f: => T): Thread = conductor.get.thread{ f }
+  protected def thread[T](name: String)(f: => T): Thread = conductor.get.thread(name){ f }
+  protected def waitForTick(tick:Int) = conductor.get.waitForTick(tick)
+  protected def tick = conductor.get.tick
+  protected implicit def addThreadsMethodToInt(nrThreads:Int) = conductor.get.addThreadsMethodToInt(nrThreads)
+
+  /**
+   * 
+   */
+  abstract override def runTest(testName: String, reporter: Reporter,
+                                stopper: Stopper, properties: Map[String,Any], tracker:Tracker) {
+
+    conductor.compareAndSet(conductor.get, new Conductor(this))
+
+    val interceptor = new PassFailInterceptor(reporter)
+
+    val startTime = System.currentTimeMillis
+
+    super.runTest(testName, interceptor, stopper, properties, tracker)
+
+    interceptor.failReport match {
+      case Some(fail) => reporter(fail)
+      case None => runConductor(testName, startTime, tracker, reporter, interceptor)
+    }
+  }
+
+  /**
+   *
+   */
+  private def runConductor(testName:String, startTime: Long, tracker: Tracker,
+                           reporter:Reporter, interceptor:PassFailInterceptor){
+
+    def testSucceededEvent = {
+      TestSucceeded(
+        tracker.nextOrdinal, suiteName,
+        Some(getClass.getName), testName,
+        Some(System.currentTimeMillis - startTime),
+        interceptor.successReport.get.formatter,
+        interceptor.successReport.get.rerunner,
+        interceptor.successReport.get.payload
+      )
+    }
+
+    def testFailedEvent(t: Throwable) = {
+      TestFailed(
+        tracker.nextOrdinal, t.getMessage, suiteName,
+        Some(getClass.getName), testName, Some(t),
+        Some(System.currentTimeMillis - startTime),
+        interceptor.successReport.get.formatter,
+        interceptor.successReport.get.rerunner,
+        interceptor.successReport.get.payload
+      )
+    }
+
+    def infoProvidedEvent(t: Throwable) = {
+      InfoProvided(
+        tracker.nextOrdinal, t.getMessage,
+        Some(NameInfo(suiteName, Some(getClass.getName), Some(testName))),
+        Some(t),
+        interceptor.successReport.get.formatter,
+        interceptor.successReport.get.payload
+      )
+    }
+
+    var caughtException = false
+
+    try {
+      conductor.get.execute()
+    } catch {
+      case e => {
+        caughtException = true
+        reporter(testFailedEvent(e))
+      }
+    } finally {
+      if (!caughtException) {
+
+        val errors = conductor.get.errors
+
+        if (errors.isEmpty) {
+          reporter(testSucceededEvent)
+        } else {
+          reporter(testFailedEvent(errors.head))
+          errors foreach{ e => reporter(infoProvidedEvent(e)) }
+        }
+      }
+    }
+  }
+  
+
+
+  /**
+   * 
+   */
+  private class PassFailInterceptor(original: Reporter) extends Reporter {
+
+    var successReport: Option[TestSucceeded] = None
+    var failReport: Option[TestFailed] = None
+
+    def apply(event:Event){
+      event match {
+        case pass:TestSucceeded => successReport = Some(pass)
+        case fail:TestFailed => failReport = Some(fail)
+        case _ => original(event)
+      }
+    }
+  }
+}
---
+++ app/src/org/scalatest/concurrent/PimpedReadWriteLock.scala
@@ -0,0 +1,77 @@
+package org.scalatest.concurrent
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joshcough
+ * Date: Jul 12, 2009
+ * Time: 1:50:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import java.util.concurrent.locks.{Lock, ReadWriteLock}
+
+/**
+ * Provides implicit def for pimping a ReadWriteLock.
+ * Provides a nice withLock method that has probably been written a million times.
+ *
+ * @author Josh Cough
+ * Date: Jun 24, 2009
+ * Time: 9:42:43 PM
+ */
+object PimpedReadWriteLock {
+  implicit def pimpMyReadWriteLock(lock: ReadWriteLock) = new PimpedReadWriteLock(lock)
+
+  /**
+   * Locks the given lock
+   * Executes the given function, holding the result
+   * Unlocks the lock
+   * Returns the result
+   *
+   * @param lock the lock to be locked while executing the given function
+   * @param f the function to be executed while the lock is locked
+   */
+  def withLock[T](lock: Lock)(f: => T): T = {
+    lock.lock
+    val t = f
+    lock.unlock
+    t
+  }
+}
+
+/**
+ * Adds withReadLock and withWriteLock functions to ReadWriteLock.
+ */
+class PimpedReadWriteLock(lock: ReadWriteLock) {
+
+  /**
+   * Locks the read lock
+   * Executes the given function, holding the result
+   * Unlocks the read lock
+   *
+   * @param f the function to be executed while the read lock is locked
+   */
+  def withReadLock[T](f: => T): T = PimpedReadWriteLock.withLock(lock.readLock) {f}
+
+  /**
+   * Alternate name for withReadLock
+   */
+  def read[T](f: => T): T = PimpedReadWriteLock.withLock(lock.readLock) {f}
+
+  /**
+   * Locks the write lock
+   * Executes the given function, holding the result
+   * Unlocks the write lock
+   * Returns the result
+   *
+   * @param f the function to be executed while the read lock is locked
+   */
+  def withWriteLock[T](f: => T): T = PimpedReadWriteLock.withLock(lock.writeLock) {f}
+
+  /**
+   * Alternate name for withWriteLock
+   * Returns the result
+   */
+  def write[T](f: => T): T = PimpedReadWriteLock.withLock(lock.writeLock) {f}
+
+}
+ 
