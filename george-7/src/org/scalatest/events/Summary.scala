10592b7c3eebd9c108f6973277bf30ed1371aafd
gcberger <gcberger@fd1274fd-080f-5963-8033-43ee40fcd1de>
2009-08-10 20:29:45 +0000

    moved george-7 from branches to branches/app
    git-svn-id: http://scalatest.googlecode.com/svn/branches/app@1700 fd1274fd-080f-5963-8033-43ee40fcd1de

---
+++ george-7/src/org/scalatest/events/Summary.scala
@@ -0,0 +1,26 @@
+package org.scalatest.events
+
+/**
+ * Class each of whose instances hold summary information about one ScalaTest run.
+ *
+ * @param testsSucceededCount the number of tests that were reported as succeeded during the run
+ * @param testsFailedCount the number of tests that were reported as failed during the run
+ * @param testsIgnoredCount the number of tests that were were reported as ignored during the run
+ * @param testsPendingCount the number of tests that were reported as pending during the run
+ *
+ * @author Bill Venners
+ */
+final case class Summary(testsSucceededCount: Int, testsFailedCount: Int, testsIgnoredCount: Int, testsPendingCount: Int,
+  suitesCompletedCount: Int, suitesAbortedCount: Int) {
+
+  /**
+   * The number of tests completed, which is the sum of the number of tests that succeeded and failed, excluding any
+   * tests that were ignored or reported as pending.
+   */
+  val testsCompletedCount = testsSucceededCount + testsFailedCount
+}
+
+/**
+ * Companion object for case class <a href="Summary.html"><code>Summary</code></a>.
+ */
+object Summary
