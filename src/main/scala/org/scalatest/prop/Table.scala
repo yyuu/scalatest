d1141ab7e11938c5a05fece8d55861065aaf35db
bv <bv@fd1274fd-080f-5963-8033-43ee40fcd1de>
2009-10-05 04:12:39 +0000

    this was what I built from for the codecamp talk
    git-svn-id: http://scalatest.googlecode.com/svn/branches/bv-codecamp2@2091 fd1274fd-080f-5963-8033-43ee40fcd1de

---
+++ src/main/scala/org/scalatest/prop/Table.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest.prop
+
+import org.scalatest._
+
+class TableFor2[A, B](heading: (String, String), rows: (A, B)*) {
+  def apply(fun: (A, B) => Unit) {
+    for ((a, b) <- rows) {
+      try {
+        fun(a, b)
+      }
+      catch {
+        case _: UnmetConditionException =>
+      }
+    }
+  }
+}
+
+class TableFor3[A, B, C](heading: (String, String, String), rows: (A, B, C)*) {
+  def apply(fun: (A, B, C) => Unit) {
+    for ((a, b, c) <- rows) {
+      try {
+        fun(a, b, c)
+      }
+      catch {
+        case _: UnmetConditionException =>
+      }
+    }
+  }
+}
+
+class TableFor4[A, B, C, D](heading: (String, String, String, String), rows: (A, B, C, D)*) {
+  def apply(fun: (A, B, C, D) => Unit) {
+    for ((a, b, c, d) <- rows) {
+      try {
+        fun(a, b, c, d)
+      }
+      catch {
+        case _: UnmetConditionException =>
+      }
+    }
+  }
+}
+
+object Table {
+  def apply[A, B](heading: (String, String), rows: (A, B)*) =
+    new TableFor2(heading, rows: _*)
+  def apply[A, B, C](heading: (String, String, String), rows: (A, B, C)*) =
+    new TableFor3(heading, rows: _*)
+  def apply[A, B, C, D](heading: (String, String, String, String), rows: (A, B, C, D)*) =
+    new TableFor4(heading, rows: _*)
+}
---
+++ src/test/scala/org/scalatest/prop/TableSpec.scala
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.scalatest.prop
+
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop._
+
+class TableSpec extends WordSpec with ShouldMatchers with PropertyChecks {
+
+  "A table" should {
+
+    "work properly with 3 columns" in {
+
+      val examples =
+        Table(
+          ("start", "eat", "left"),
+          (     12,     5,      7),
+          (     12,     5,      8), // Should fail
+          (     20,     5,     15)
+        )
+
+      evaluating {
+        forAll (examples) { (start: Int, eat: Int, left: Int) =>
+          start - eat should equal (left)
+        }
+      } should produce [TestFailedException]
+
+      forAll (examples) { (start: Int, eat: Int, left: Int) =>
+        whenever (left != 8) {
+          start - eat should equal (left)
+        }
+      }
+    }
+
+    "work properly with 4 columns" in {
+
+      val examples =
+        Table(
+          (     "n",     "d", "numer", "denom"),
+          (       1,       2,       1,       2),
+          (       1,       2,       1,       3), // Should fail
+          (      -1,       2,      -1,       2),
+          (       1,      -2,       1,      -2)
+        )
+
+      evaluating {
+        forAll (examples) { (n: Int, d: Int, numer: Int, denom: Int) =>
+          val r = new Rational(n, d)
+          r.numer should equal (numer)
+          r.denom should equal (denom)
+        }
+      } should produce [TestFailedException]
+
+      forAll (examples) { (n: Int, d: Int, numer: Int, denom: Int) =>
+        whenever (denom != 3) {
+          val r = new Rational(n, d)
+          r.numer should equal (numer)
+          r.denom should equal (denom)
+        }
+      }
+    }
+
+    "work properly with 2 columns" in {
+
+      val examples =
+        Table(
+          (   "n",   "d"),
+          (     1,     2),
+          (    -1,     0), // Should fail
+          (    -1,     2),
+          (     1,    -2)
+        )
+
+      evaluating {
+        forAll (examples) { (n: Int, d: Int) =>
+          def gcd(a: Int, b: Int): Int =
+            if (b == 0) a else gcd(b, a % b)
+          val g = gcd(n.abs, d.abs)
+          val r = new Rational(n, d)
+          r.numer should equal (n / g)
+          r.denom should equal (d / g)
+        }
+      } should produce [IllegalArgumentException]
+
+      forAll (examples) { (n: Int, d: Int) =>
+        whenever (d != 0) {
+          def gcd(a: Int, b: Int): Int =
+            if (b == 0) a else gcd(b, a % b)
+          val g = gcd(n.abs, d.abs)
+          val r = new Rational(n, d)
+          r.numer should equal (n / g)
+          r.denom should equal (d / g)
+        }
+      }
+    }
+
+    "work properly with no columns" ignore {
+
+      forAll { (n: Int, d: Int) =>
+        whenever (d != 0) {
+          def gcd(a: Int, b: Int): Int =
+            if (b == 0) a else gcd(b, a % b)
+          val g = gcd(n.abs, d.abs)
+          val r = new Rational(n, d)
+          r.numer should equal (n / g)
+          r.denom should equal (d / g)
+        }
+      }
+    }
+  }
+}
