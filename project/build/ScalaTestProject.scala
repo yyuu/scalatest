b4a33e5e9ce251cbd6d46048a7a924190f658fc6
joshcough <joshcough@fd1274fd-080f-5963-8033-43ee40fcd1de>
2009-11-06 17:23:42 +0000

    added several tests, added more dependencies to sbt; lib dir is no longer needed.
    git-svn-id: http://scalatest.googlecode.com/svn/branches/josh-sbt-interfaces@2137 fd1274fd-080f-5963-8033-43ee40fcd1de

--- project/build/ScalaTestProject.scala
+++ project/build/ScalaTestProject.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2001-2008 Artima, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import sbt._
 
 class ScalaTestProject(info: ProjectInfo) extends ParentProject(info) {
@@ -7,7 +22,7 @@ class ScalaTestProject(info: ProjectInfo) extends ParentProject(info) {
   lazy val must = project("app" / "must_matchers", "must_matchers", new MustMatchers(_), main)
 
   class ScalaTestParentProject(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins {
-    override def crossScalaVersions = Set("2.7.5")
+    //override def crossScalaVersions = Set("2.7.5")
     val sxr = compilerPlugin("org.scala-tools.sxr" %% "sxr" % "0.2.1")
     // puts sbt on the classpath
     override def compileClasspath = super.compileClasspath +++ Path.fromFile(FileUtilities.sbtJar)
@@ -25,9 +40,13 @@ class ScalaTestProject(info: ProjectInfo) extends ParentProject(info) {
     val ant = "org.apache.ant" % "ant" % "1.7.1"
     val hamcrest = "org.hamcrest" % "hamcrest-all" % "1.1"
     val scalacheck = "org.scala-tools.testing" % "scalacheck" % "1.5"
+    val testInterfaces = "org.scala-tools.testing" % "test-interface" % "0.1"
     val mockito = "org.mockito" % "mockito-all" % "1.7"
-    val jmock = "org.jmock" % "jmock" % "2.5.1"
+    val jmock = "org.jmock" % "jmock-legacy" % "2.5.1"
+    val jmockLegacy = "org.jmock" % "jmock" % "2.5.1"
     val easymock = "org.easymock" % "easymock" % "2.5.1"
+    val easymockClassExtension = "org.easymock" % "easymockclassextension" % "2.4"
+    val commonsIo = "commons-io" % "commons-io" % "1.3.2"
 
    /** cobertura.jar   code coverage ... **/
 
