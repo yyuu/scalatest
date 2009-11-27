73bc7d48a5b05f6c8c03274744c6546c9fa75f67
joshcough <joshcough@fd1274fd-080f-5963-8033-43ee40fcd1de>
2009-11-27 00:48:23 +0000

    upgraded to test-interfaces 0.2
    git-svn-id: http://scalatest.googlecode.com/svn/branches/app@2177 fd1274fd-080f-5963-8033-43ee40fcd1de

---
+++ josh-sbt-interfaces-0.2/src/main/scala/org/scalatest/tools/ScalaTestFramework.scala
@@ -0,0 +1,17 @@
+package org.scalatest.tools
+
+import org.scalatools.testing.{Logger, Framework}
+
+class ScalaTestFramework extends Framework
+{
+  def name = "ScalaTest"
+  
+  def tests = Array(new org.scalatools.testing.TestFingerprint{
+    def superClassName = "org.scalatest.Suite"
+    def isModule = false
+  })
+
+  def  testRunner(testLoader: ClassLoader, loggers: Array[Logger]) = {
+    new ScalaTestRunner(testLoader, loggers)
+  }
+}
