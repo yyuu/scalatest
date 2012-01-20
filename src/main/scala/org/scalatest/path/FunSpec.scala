b3e3fdf246a7fa45f41683f3da248a7a30b95772
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-01-20 09:55:20 +0000

    1.8 Branch - Setting path and being picky about describe.
    git-svn-id: http://scalatest.googlecode.com/svn/branches/to-release-as-1.8@3279 fd1274fd-080f-5963-8033-43ee40fcd1de

--- src/main/scala/org/scalatest/path/FunSpec.scala
+++ src/main/scala/org/scalatest/path/FunSpec.scala
@@ -1,10 +1,329 @@
 package org.scalatest.path
+import org.scalatest.OneInstancePerTest
+import org.scalatest.Reporter
+import org.scalatest.Stopper
+import org.scalatest.Filter
+import org.scalatest.Tracker
+import org.scalatest.Distributor
+import org.scalatest.Engine
+import org.scalatest.Informer
+import org.scalatest.Tag
+import org.scalatest.verb.BehaveWord
+import scala.collection.immutable.ListSet
 
-trait FunSpec extends org.scalatest.FunSpec {
+trait FunSpec extends org.scalatest.Suite with OneInstancePerTest { thisSuite =>
   
+  import FunSpec.getPath  
+  private val targetPath = getPath
   
+  private final val engine = new Engine("concurrentSpecMod", "Spec")
+  import engine._
   
+  protected[scalatest] val fileName = "FunSpec.scala"
 
+  /**
+   * Returns an <code>Informer</code> that during test execution will forward strings (and other objects) passed to its
+   * <code>apply</code> method to the current reporter. If invoked in a constructor, it
+   * will register the passed string for forwarding later during test execution. If invoked while this
+   * <code>FunSpec</code> is being executed, such as from inside a test function, it will forward the information to
+   * the current reporter immediately. If invoked at any other time, it will
+   * throw an exception. This method can be called safely by any thread.
+   */
+  implicit protected def info: Informer = atomicInformer.get
+
+  /**
+   * Class that, via an instance referenced from the <code>it</code> field,
+   * supports test (and shared test) registration in <code>FunSpec</code>s.
+   *
+   * <p>
+   * This class supports syntax such as the following test registration:
+   * </p>
+   *
+   * <pre class="stExamples">
+   * it("should be empty")
+   * ^
+   * </pre>
+   *
+   * <p>
+   * and the following shared test registration:
+   * </p>
+   *
+   * <pre class="stExamples">
+   * it should behave like nonFullStack(stackWithOneItem)
+   * ^
+   * </pre>
+   *
+   * <p>
+   * For more information and examples, see the <a href="FunSpec.html">main documentation for <code>FunSpec</code></a>.
+   * </p>
+   */
+  protected class ItWord {
+
+    /**
+     * Register a test with the given spec text, optional tags, and test function value that takes no arguments.
+     * An invocation of this method is called an &#8220;example.&#8221;
+     *
+     * This method will register the test for later execution via an invocation of one of the <code>execute</code>
+     * methods. The name of the test will be a concatenation of the text of all surrounding describers,
+     * from outside in, and the passed spec text, with one space placed between each item. (See the documenation
+     * for <code>testNames</code> for an example.) The resulting test name must not have been registered previously on
+     * this <code>FunSpec</code> instance.
+     *
+     * @param specText the specification text, which will be combined with the descText of any surrounding describers
+     * to form the test name
+     * @param testTags the optional list of tags for this test
+     * @param testFun the test function
+     * @throws DuplicateTestNameException if a test with the same name has been registered previously
+     * @throws TestRegistrationClosedException if invoked after <code>run</code> has been invoked on this suite
+     * @throws NullPointerException if <code>specText</code> or any passed test tag is <code>null</code>
+     */
+    def apply(specText: String, testTags: Tag*)(testFun: => Unit) {
+      registerTest(specText, testFun _, "itCannotAppearInsideAnotherIt", fileName, "apply", testTags: _*)
+    }
+
+    /**
+     * Supports the registration of shared tests.
+     *
+     * <p>
+     * This method supports syntax such as the following:
+     * </p>
+     *
+     * <pre class="stExamples">
+     * it should behave like nonFullStack(stackWithOneItem)
+     *    ^
+     * </pre>
+     *
+     * <p>
+     * For examples of shared tests, see the <a href="FunSpec.html#SharedTests">Shared tests section</a>
+     * in the main documentation for trait <code>FunSpec</code>.
+     * </p>
+     */
+    def should(behaveWord: BehaveWord) = behaveWord
+
+    /**
+     * Supports the registration of shared tests.
+     *
+     * <p>
+     * This method supports syntax such as the following:
+     * </p>
+     *
+     * <pre class="stExamples">
+     * it must behave like nonFullStack(stackWithOneItem)
+     *    ^
+     * </pre>
+     *
+     * <p>
+     * For examples of shared tests, see the <a href="FunSpec.html#SharedTests">Shared tests section</a>
+     * in the main documentation for trait <code>FunSpec</code>.
+     * </p>
+     */
+    def must(behaveWord: BehaveWord) = behaveWord
+  }
+
+  /**
+   * Supports test (and shared test) registration in <code>FunSpec</code>s.
+   *
+   * <p>
+   * This field supports syntax such as the following:
+   * </p>
+   *
+   * <pre class="stExamples">
+   * it("should be empty")
+   * ^
+   * </pre>
+   *
+   * <pre> class="stExamples"
+   * it should behave like nonFullStack(stackWithOneItem)
+   * ^
+   * </pre>
+   *
+   * <p>
+   * For more information and examples of the use of the <code>it</code> field, see the main documentation for this trait.
+   * </p>
+   */
+  protected val it = new ItWord
+
+  /**
+   * Register a test to ignore, which has the given spec text, optional tags, and test function value that takes no arguments.
+   * This method will register the test for later ignoring via an invocation of one of the <code>execute</code>
+   * methods. This method exists to make it easy to ignore an existing test by changing the call to <code>it</code>
+   * to <code>ignore</code> without deleting or commenting out the actual test code. The test will not be executed, but a
+   * report will be sent that indicates the test was ignored. The name of the test will be a concatenation of the text of all surrounding describers,
+   * from outside in, and the passed spec text, with one space placed between each item. (See the documenation
+   * for <code>testNames</code> for an example.) The resulting test name must not have been registered previously on
+   * this <code>FunSpec</code> instance.
+   *
+   * @param specText the specification text, which will be combined with the descText of any surrounding describers
+   * to form the test name
+   * @param testTags the optional list of tags for this test
+   * @param testFun the test function
+   * @throws DuplicateTestNameException if a test with the same name has been registered previously
+   * @throws TestRegistrationClosedException if invoked after <code>run</code> has been invoked on this suite
+   * @throws NullPointerException if <code>specText</code> or any passed test tag is <code>null</code>
+   */
+  protected def ignore(testText: String, testTags: Tag*)(testFun: => Unit) {
+    registerIgnoredTest(testText, testFun _, "ignoreCannotAppearInsideAnIt", fileName, "ignore", testTags: _*)
+  }
+  
+  import scala.collection.mutable.ListBuffer
+  private var currentPath = List.empty[Int]
+  private var usedPathSet = Set.empty[String]
+  
+  private def getNextPath() = {
+    var next: List[Int] = null
+    var count = 0
+    while (next == null) {
+      val candidate = currentPath ::: List(count)
+      if (!usedPathSet.contains(candidate.toList.toString)) {
+        next = candidate
+        usedPathSet += candidate.toList.toString
+      }
+      else
+        count += 1
+    }
+    next
+  }
+  
+  private def isInTargetPath(currentPath: List[Int], targetPath: List[Int]): Boolean = {
+    targetPath.take(currentPath.length) == currentPath
+  }
+
+  /**
+   * Describe a &#8220;subject&#8221; being specified and tested by the passed function value. The
+   * passed function value may contain more describers (defined with <code>describe</code>) and/or tests
+   * (defined with <code>it</code>). This trait's implementation of this method will register the
+   * description string and immediately invoke the passed function.
+   */
+  protected def describe(description: String)(fun: => Unit) {
+    val nextPath = getNextPath()
+    val a = targetPath
+    val b = usedPathSet
+    if (!targetPath.isDefined || isInTargetPath(nextPath, targetPath.get)) {
+      val oldCurrentPath = currentPath
+      currentPath = nextPath
+      registerNestedBranch(description, None, fun, "describeCannotAppearInsideAnIt", fileName, "describe")
+      currentPath = oldCurrentPath
+    }
+  }
+
+  /**
+   * An immutable <code>Set</code> of test names. If this <code>FunSpec</code> contains no tests, this method returns an
+   * empty <code>Set</code>.
+   *
+   * <p>
+   * This trait's implementation of this method will return a set that contains the names of all registered tests. The set's
+   * iterator will return those names in the order in which the tests were registered. Each test's name is composed
+   * of the concatenation of the text of each surrounding describer, in order from outside in, and the text of the
+   * example itself, with all components separated by a space. For example, consider this <code>FunSpec</code>:
+   * </p>
+   *
+   * <pre class="stHighlight">
+   * import org.scalatest.FunSpec
+   *
+   * class StackSpec extends FunSpec {
+   *   describe("A Stack") {
+   *     describe("(when not empty)") {
+   *       it("must allow me to pop") {}
+   *     }
+   *     describe("(when not full)") {
+   *       it("must allow me to push") {}
+   *     }
+   *   }
+   * }
+   * </pre>
+   *
+   * <p>
+   * Invoking <code>testNames</code> on this <code>FunSpec</code> will yield a set that contains the following
+   * two test name strings:
+   * </p>
+   *
+   * <pre class="stExamples">
+   * "A Stack (when not empty) must allow me to pop"
+   * "A Stack (when not full) must allow me to push"
+   * </pre>
+   */
+  override def testNames: Set[String] = {
+    // I'm returning a ListSet here so that they tests will be run in registration order
+    ListSet(atomic.get.testNamesList.toArray: _*)
+  }
+
+  /**
+   * Run a test. This trait's implementation runs the test registered with the name specified by
+   * <code>testName</code>. Each test's name is a concatenation of the text of all describers surrounding a test,
+   * from outside in, and the test's  spec text, with one space placed between each item. (See the documenation
+   * for <code>testNames</code> for an example.)
+   *
+   * @param testName the name of one test to execute.
+   * @param reporter the <code>Reporter</code> to which results will be reported
+   * @param stopper the <code>Stopper</code> that will be consulted to determine whether to stop execution early.
+   * @param configMap a <code>Map</code> of properties that can be used by this <code>FunSpec</code>'s executing tests.
+   * @throws NullPointerException if any of <code>testName</code>, <code>reporter</code>, <code>stopper</code>, or <code>configMap</code>
+   *     is <code>null</code>.
+   */
+  protected override def runTest(testName: String, reporter: Reporter, stopper: Stopper, configMap: Map[String, Any], tracker: Tracker) {
+
+    def invokeWithFixture(theTest: TestLeaf) {
+      val theConfigMap = configMap
+      withFixture(
+        new NoArgTest {
+          def name = testName
+          def apply() { theTest.testFun() }
+          def configMap = theConfigMap
+        }
+      )
+    }
+
+    runTestImpl(thisSuite, testName, reporter, stopper, configMap, tracker, true, invokeWithFixture)
+  }
+
+  /**
+   * A <code>Map</code> whose keys are <code>String</code> tag names to which tests in this <code>FunSpec</code> belong, and values
+   * the <code>Set</code> of test names that belong to each tag. If this <code>FunSpec</code> contains no tags, this method returns an empty <code>Map</code>.
+   *
+   * <p>
+   * This trait's implementation returns tags that were passed as strings contained in <code>Tag</code> objects passed to 
+   * methods <code>test</code> and <code>ignore</code>. 
+   * </p>
+   */
+  override def tags: Map[String, Set[String]] = atomic.get.tagsMap
+
+  override def run(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
+      configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
+
+    runImpl(thisSuite, testName, reporter, stopper, filter, configMap, distributor, tracker, super.run)
+  }
+
+  /**
+   * Supports shared test registration in <code>FunSpec</code>s.
+   *
+   * <p>
+   * This field supports syntax such as the following:
+   * </p>
+   *
+   * <pre class="stExamples">
+   * it should behave like nonFullStack(stackWithOneItem)
+   *           ^
+   * </pre>
+   *
+   * <p>
+   * For more information and examples of the use of <cod>behave</code>, see the <a href="#SharedTests">Shared tests section</a>
+   * in the main documentation for this trait.
+   * </p>
+   */
+  protected val behave = new BehaveWord
+
+  protected override def runTests(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
+                             configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
+    testName match {
+      case Some(tn) => super.runTests(testName, reporter, stopper, filter, configMap, None, tracker)
+      case None =>
+        for (tn <- testNames) {
+          FunSpec.setPath(engine.testPath(tn))
+          val oneInstance = newInstance
+          oneInstance.run(Some(tn), reporter, stopper, filter, configMap, None, tracker)
+        }
+    }
+  }
 }
 
 private[path] object FunSpec {
--- src/test/scala/org/scalatest/path/FunSpecSpec.scala
+++ src/test/scala/org/scalatest/path/FunSpecSpec.scala
@@ -2,8 +2,12 @@ package org.scalatest.path
 
 import org.scalatest.PrivateMethodTester
 import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.SharedHelpers
+import org.scalatest.Stopper
+import org.scalatest.Filter
+import org.scalatest.Tracker
 
-class FunSpecSpec extends org.scalatest.FunSpec with ShouldMatchers with PrivateMethodTester {
+class FunSpecSpec extends org.scalatest.FunSpec with ShouldMatchers with SharedHelpers with PrivateMethodTester {
 
   describe("FunSpec ThreadLocal variable") {
     it("should be set by setPath and clear by getPath") {
@@ -13,6 +17,65 @@ class FunSpecSpec extends org.scalatest.FunSpec with ShouldMatchers with Private
       FunSpec invokePrivate setPath(List(1, 2, 3))
       FunSpec invokePrivate getPath() should be (Some(List(1, 2, 3)))
       FunSpec invokePrivate getPath() should be (None)
+    }
+  }
+  
+  class MyFunSpec extends org.scalatest.path.FunSpec with ShouldMatchers {
+    
+    import scala.collection.mutable.ListBuffer
+    import MyFunSpec._
+    
+    count += 1 
+    //val getPath = PrivateMethod[Option[List[Int]]]('getPath)
+    //pathBuf += FunSpec invokePrivate getPath() 
+    
+    describe("An empty list") {
+      val list = ListBuffer[Int]() 
+      
+      describe("when 1 is inserted") {
+        it("should have only 1 in it") {
+          list += 1 
+          list should be (ListBuffer(1)) 
+        }
+        firstDescCount += 1
+      }
+      
+      describe("when 2 is inserted") {
+        it("should have only 2 in it") {
+          list += 2
+          list should be (ListBuffer(2))
+        }
+        secondDescCount += 1
+      }
+      outerDescCount += 1
+    }
+    
+    override def newInstance = new MyFunSpec
+  }
+  
+  object MyFunSpec {
+    import scala.collection.mutable.ListBuffer
+    
+    var count = 0
+    //val pathBuf = ListBuffer.empty[Option[List[Int]]]
+    var firstDescCount = 0
+    var secondDescCount = 0
+    var outerDescCount = 0
+  }
+  
+  describe("FunSpec") {
+    
+    it("should create one instance per test +1") {
+      val mySpec = new MyFunSpec()
+      mySpec.run(None, SilentReporter, new Stopper {}, Filter(), Map(), None, new Tracker())
+      assert(MyFunSpec.count === 3)
+      //assert(MyFunSpec.pathBuf.size === 3)
+      //assert(MyFunSpec.pathBuf.exists(_ == Some(List(0, 0, 0))))
+      //assert(MyFunSpec.pathBuf.exists(_ == Some(List(0, 1, 0))))
+      //assert(MyFunSpec.pathBuf.exists(_ == None))
+      assert(MyFunSpec.firstDescCount === 2)
+      assert(MyFunSpec.secondDescCount === 2)
+      assert(MyFunSpec.outerDescCount === 3)
     }
   }
   
