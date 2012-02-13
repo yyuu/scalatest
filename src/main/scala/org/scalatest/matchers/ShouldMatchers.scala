92e131a27dab4c441c120a4fe9eedef30f3d999e
billvenners@gmail.com <billvenners@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-02-13 16:46:34 +0000

    Changed shouldEqual to shouldBe.
    git-svn-id: http://scalatest.googlecode.com/svn/branches/to-release-as-1.8@3356 fd1274fd-080f-5963-8033-43ee40fcd1de

--- src/main/scala/org/scalatest/matchers/ShouldMatchers.scala
+++ src/main/scala/org/scalatest/matchers/ShouldMatchers.scala
@@ -926,14 +926,14 @@ trait ShouldMatchers extends Matchers with ShouldVerb {
      * This method enables syntax such as the following:
      *
      * <pre class="stHighlight">
-     * result shouldEqual 3
+     * result shouldBe 3
      *        ^
      * </pre>
      */
-    def shouldEqual(right: Any) {
+    def shouldBe(right: Any) {
       if (!areEqualComparingArraysStructurally(left, right)) {
         val (leftee, rightee) = Suite.getObjectsForFailureMessage(left, right)
-        throw newTestFailedException(FailureMessages("didNotEqual", leftee, rightee))
+        throw newTestFailedException(FailureMessages("wasNotEqualTo", leftee, rightee))
       }
     }
   }
