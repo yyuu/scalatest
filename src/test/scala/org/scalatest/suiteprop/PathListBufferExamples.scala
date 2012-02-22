cf10d6a02c5c1bc14a615c048bf7465e89f8e22b
billvenners@gmail.com <billvenners@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-02-22 19:28:39 +0000

    A few more ListBuffer examples.
    git-svn-id: http://scalatest.googlecode.com/svn/branches/to-release-as-1.8@3468 fd1274fd-080f-5963-8033-43ee40fcd1de

--- src/test/scala/org/scalatest/suiteprop/PathListBufferExamples.scala
+++ src/test/scala/org/scalatest/suiteprop/PathListBufferExamples.scala
@@ -217,6 +217,9 @@ class PathListBufferExamples extends PathSuiteExamples {
       describe("A subject") {
       }
       describe("Another subject") {
+        it("first test") {
+          buf += 1000 // To ensure this isn't seen by others
+        }
       }
       it("should be empty when created") {
         buf should be ('empty)
@@ -276,7 +279,7 @@ class PathListBufferExamples extends PathSuiteExamples {
   }
 
   class PathFunSpecExample(val counts: Counts) extends path.FunSpec with Services with ShouldMatchers {
-
+// todo change to var
     counts.instanceCount += 1
     val expectedInstanceCount = 7
    
@@ -432,11 +435,10 @@ class PathListBufferExamples extends PathSuiteExamples {
     counts.instanceCount += 1
     val expectedInstanceCount = 8
    
-    "A subject" - {
-    }
-
     "A ListBuffer" - {
       val buf = ListBuffer.empty[Int]
+      "A subject" - {
+      }
       "should be empty when created" in {
         buf should be ('empty)
         buf += 99 // Mutate to make sure no other test sees this
@@ -491,13 +493,12 @@ class PathListBufferExamples extends PathSuiteExamples {
     counts.instanceCount += 1
     val expectedInstanceCount = 9
    
-    "A subject" - {
-    }
-    "Another subject" - {
-    }
-
     "A ListBuffer" - {
       val buf = ListBuffer.empty[Int]
+      "A subject" - {
+      }
+      "Another subject" - {
+      }
       "should be empty when created" in {
         buf should be ('empty)
         buf += 99 // Mutate to make sure no other test sees this
@@ -550,8 +551,64 @@ class PathListBufferExamples extends PathSuiteExamples {
   class OneTestSiblingEmptyNestedPathFreeSpecExample(val counts: Counts) extends path.FreeSpec with Services with ShouldMatchers {
 
     counts.instanceCount += 1
-    val expectedInstanceCount = 1
+    val expectedInstanceCount = 9
    
+   
+    "A ListBuffer" - {
+      val buf = ListBuffer.empty[Int]
+      "A subject" - {
+      }
+      "Another subject" - {
+        "first test" - {
+          buf += 1000 // To ensure this isn't seen by others
+        }
+      }
+      "should be empty when created" in {
+        buf should be ('empty)
+        buf += 99 // Mutate to make sure no other test sees this
+      }
+      "when 1 is appended" - {
+        buf += 1
+        "should contain 1" in {
+          buf should equal (Seq(1))
+          buf += 99 // Mutate to make sure no other test sees this
+        }
+        "when 2 is appended" - {
+          buf += 2
+          "should contain 1 and 2" in {
+            buf should equal (Seq(1, 2))
+            buf += 99 // Mutate to make sure no other test sees this
+          }
+          "when 2 is removed" - {
+            buf -= 2
+            "should contain only 1 again" in {
+              buf should equal (Seq(1))
+              buf += 99 // Mutate to make sure no other test sees this
+            }
+          }
+          "when 3 is appended" - { // This describe should not see the removal of 2 done in earlier sibling describe
+            buf += 3
+            "should contain 1, 2, and 3" in {
+              buf should equal (Seq(1, 2, 3))
+              buf += 99 // Mutate to make sure no other test sees this
+            }
+          }
+        }
+        "when 88 is appended" - {
+          buf += 88
+          "should contain 1 and 88" in {
+            buf should equal (Seq(1, 88))
+            buf += 99 // Mutate to make sure no other test sees this
+          }
+        }
+      }
+      // At end of previous describe, buf equaled List(1). Now doing it again to make
+      // sure that it is empty
+      "should again be empty" in {
+        buf should be ('empty)
+      }
+    }
+
     override def newInstance = new OneTestSiblingEmptyNestedPathFreeSpecExample(counts)
   }
   
