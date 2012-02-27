4501eef34a7cba50d70b821866ce7c29e189f259
chuacheeseng83@gmail.com <chuacheeseng83@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-02-27 09:15:57 +0000

    locspi branch - Annotated Spec style trait with @Style("org.scalatest.finders.FunSpecFinder").
    git-svn-id: http://scalatest.googlecode.com/svn/branches/locspi@3519 fd1274fd-080f-5963-8033-43ee40fcd1de

--- src/main/scala/org/scalatest/Spec.scala
+++ src/main/scala/org/scalatest/Spec.scala
@@ -1259,6 +1259,7 @@ import Suite.reportTestIgnored
  *
  * @author Bill Venners
  */
+@Style("org.scalatest.finders.FunSpecFinder")
 trait Spec extends Suite { thisSuite =>
 
   private final val engine = new Engine("concurrentSpecMod", "Spec")
