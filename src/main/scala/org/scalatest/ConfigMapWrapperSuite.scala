edcdd93bfeab080c598cbd00432a5f298cabbe26
billvenners@gmail.com <billvenners@gmail.com@fd1274fd-080f-5963-8033-43ee40fcd1de>
2012-03-07 06:48:51 +0000

    Added a commment.
    git-svn-id: http://scalatest.googlecode.com/svn/branches/to-release-as-1.8@3557 fd1274fd-080f-5963-8033-43ee40fcd1de

--- src/main/scala/org/scalatest/ConfigMapWrapperSuite.scala
+++ src/main/scala/org/scalatest/ConfigMapWrapperSuite.scala
@@ -1,5 +1,11 @@
 package org.scalatest
 
+// TODO: Need to create a separate instance when expectedTestCount is invoked, testNames, nestedSuites, and tags, etc.
+// Could memoize that instance I suppose. a lazy one for all non-run method invocations.
+/**
+ * Wrapper <code>Suite</code> that passes an instance of the config map to the constructor of the
+ * wrapped <code>Suite</code> when <code>run</code> is invoked.
+ */
 final class ConfigMapWrapperSuite(clazz: Class[_ <: Suite]) extends Suite {
   override def run(testName: Option[String], reporter: Reporter, stopper: Stopper, filter: Filter,
       configMap: Map[String, Any], distributor: Option[Distributor], tracker: Tracker) {
